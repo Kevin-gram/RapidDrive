5a0934a5cf85f8796712ad14f770d3c39ee6406e
kevin-gram <k.nyiringan@alustudent.com>
2024-04-01 22:04:10 +0200

    DashBoard

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -6,8 +6,8 @@ import ImageD from '../images/Flag_of_the_United_Nations.svg.png';
 
 export default function Companies() {
   return (
-    <div className='bg-navy pb-10 sm:pt-24 md:pt-32 pt-52'>
-      <h1 className='text-xl sm:text-3xl flex justify-center font-bold font-xyz  text-white py-10 md:mt-10'> Our Partners </h1>
+    <div className='bg-navy pb-10 sm:pt-24 md:pt-32 pt-72'>
+      <h1 className='sm:text-xl flex justify-center font-bold font-xyz text-3xl text-white py-10'> Our Partners </h1>
       <div className='flex flex-wrap justify-evenly overflow-hidden'>
         <img src={ImageA} alt="Example Image" className='w-1/7 h-auto max-w-[150px] max-h-[150px] rounded-xl mb-5' />
         <img src={ImageB} alt="Example Image" className='w-1/7 h-auto max-w-[150px] max-h-[150px] rounded-xl mb-5' />
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -40,7 +40,7 @@ function Testimonials() {
   return (
     <section className="py-12 bg-gradient-to-b from-navy to-dark">
       <div className="container mx-auto">
-        <h2 className="text-3xl font-bold text-center mb-8 font-xyz text-white">What People Are Saying</h2>
+        <h2 className="text-2xl font-bold text-center mb-8 font-xyz text-white">What People Are Saying</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {testimonials.map((testimonial, index) => (
             <TestimonialCard key={index} {...testimonial} />
